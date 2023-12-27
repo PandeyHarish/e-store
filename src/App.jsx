c705576a42a135eba820f7ccf6616655d319e541
Harish Pandey <pandeyharish394@gmail.com>
2023-12-27 19:14:39 +0545

    setup of reacticon and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar/>
     </div>
   );
 };
